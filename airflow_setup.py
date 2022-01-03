573486659a1c84264a4313f832db6cd6645aab0d
C. Thomas Brittain <honeysucklehardware@gmail.com>
2022-01-03 13:19:20 -0600

    Working

--- airflow_setup.py
+++ airflow_setup.py
@@ -1,6 +1,6 @@
 import os
 import argparse
-from rich import print as rprint
+import subprocess
 
 ###############
 # Parse args
@@ -25,50 +25,55 @@ VIRT_ENV_NAME = "sandbox"
 STABLE_HELM_REPO = "https://charts.helm.sh/stable"
 USER_NAME = "af_workstation"
 
-MSG_COLOR = "magenta"
-
+MSG_COLOR_START = "\\033[0;35;40m"
+MSG_COLOR_STOP = "\\033[0;0m'"
+    
+def exec(cmd: str) -> None:
+    subprocess.call(cmd)
+    
 def print(text: str):
-    rprint(f"[{MSG_COLOR}]{text}[/{MSG_COLOR}]")
+    rprint(f"echo '{MSG_COLOR_START}{text}{MSG_COLOR_STOP}'")
 
 ################################
 # Switch to user dir
 ################################
-os.system(f"cd /home/{OS_USER_NAME}")
+exec(f"cd /home/{OS_USER_NAME}")
+
 
 ######################################
 # Configure AWS CLI
 ######################################
-os.system(f"aws configure set aws_access_key_id {AWS_ACCOUNT_ID}")
-os.system(f"aws configure set aws_secret_access_key {AWS_SECRET_ACCESS_KEY}")
-os.system(f"aws configure set region {AWS_REGION}")
-os.system(f"""echo 'export ACCOUNT_ID={AWS_ACCOUNT_ID}' | tee -a ~/.bash_profile""")
-os.system(f"""echo 'export AWS_REGION={AWS_REGION}' | tee -a ~/.bash_profile""")
+exec(f"aws configure set aws_access_key_id {AWS_ACCOUNT_ID}")
+exec(f"aws configure set aws_secret_access_key {AWS_SECRET_ACCESS_KEY}")
+exec(f"aws configure set region {AWS_REGION}")
+exec(f"""echo 'export ACCOUNT_ID={AWS_ACCOUNT_ID}' | tee -a ~/.bash_profile""")
+exec(f"""echo 'export AWS_REGION={AWS_REGION}' | tee -a ~/.bash_profile""")
 
 ##############################
 # Setup virtual Environment
 ##############################
 print(f"Setting up virtual environment: {VIRT_ENV_NAME}")
 print(f"Activate with: source .{VIRT_ENV_NAME}/bin/activate")
-os.system("pip3 install --upgrade pip --user")
-os.system(f"python3 -m venv .{VIRT_ENV_NAME}")
-os.system(f"source .{VIRT_ENV_NAME}/bin/activate")
+exec("pip3 install --upgrade pip --user")
+exec(f"python3 -m venv .{VIRT_ENV_NAME}")
+exec(f"source .{VIRT_ENV_NAME}/bin/activate")
 
 ############################################
 # Setup eksctl for managing Kubernetes
 ############################################
 print("Installing eksctl")
 print("For info visit: https://eksctl.io/")
-os.system("""curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/0.23.0/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp """)
-os.system("sudo mv /tmp/eksctl /usr/local/bin")
+exec("""curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/0.23.0/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp """)
+exec("sudo mv /tmp/eksctl /usr/local/bin")
 
 ############################################
 # Setup kubectl
 ############################################
 print("Installing kubectl")
 print("For info visit: https://kubernetes.io/docs/tasks/tools/")
-os.system("""sudo curl --silent --location https://storage.googleapis.com/kubernetes-release/release/v1.18.2/bin/linux/amd64/kubectl """)
-os.system("sudo chmod +x ./kubectl")
-os.system("sudo mv ./kubectl /usr/local/bin/kubectl")
+exec("""sudo curl --silent --location https://storage.googleapis.com/kubernetes-release/release/v1.18.2/bin/linux/amd64/kubectl """)
+exec("sudo chmod +x ./kubectl")
+exec("sudo mv ./kubectl /usr/local/bin/kubectl")
 
 ###################
 # Install Helm3
@@ -76,21 +81,21 @@ os.system("sudo mv ./kubectl /usr/local/bin/kubectl")
 ###################
 print("Installing helm")
 print("For info visit: https://helm.sh/")
-os.system("curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash")
+exec("curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash")
 
 print(f"Adding stable Helm repository: {STABLE_HELM_REPO}")
-os.system(f"helm repo add stable {STABLE_HELM_REPO}")
+exec(f"helm repo add stable {STABLE_HELM_REPO}")
 
 
 ###################
 # Setup git
 ###################
 print(f"Setting git user.name to '{USER_NAME}'")
-os.system(f"git config --global user.name '{USER_NAME}'")
+exec(f"git config --global user.name '{USER_NAME}'")
 
 ###################
 # Upgrade AWS CLI Tools
 ###################
 print("Upgrading AWS CLI and adding additional tooling.")
-os.system("pip3 install --upgrade awscli --user && hash -r")
-os.system("sudo yum -y install jq gettext bash-completion moreutils")
+exec("pip3 install --upgrade awscli --user && hash -r")
+exec("sudo yum -y install jq gettext bash-completion moreutils")
